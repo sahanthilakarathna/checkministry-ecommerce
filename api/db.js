7cc084df688871a85ca337bc50a0b5bcb74562b0
sahanNeosolax <sahan.thilakarathna@neosolax.com>
2025-12-09 19:35:30 +0530

    db

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Sahan@123@localhost:5432/ecommerce'
+  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_3WQhVtKPJzn8@ep-tiny-haze-aec64ubm-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 });
 
 module.exports = {
